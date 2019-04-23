e4a8eee969274fd13880c2d91039652b0bec722e
marcela garizao <marchsantrich@hotmail.com>
2019-04-23 15:40:59 -0500

    updates on products update route and app basics

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var routes = require('./routes');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug')
- 
+
+app.use(express.json());
 app.use('/', routes);
- 
+
 app.listen(8080 );
 
 
--- routes/products/index.js
+++ routes/products/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var products = express.Router();
 var fs = require('fs');
 
-
 /* GET products listing. */
 products.get('/', function(req, res) {
   fs.readFile('./files/default_storage.json', function(err, data) {
@@ -10,10 +9,11 @@ products.get('/', function(req, res) {
   });
 });
 
+/* PUT products update. */
 products.put('/', function(req, res){
-   fs.writeFile('./files/default_storage.json', req.params.products, function (err) {
-      res.status(200).json({ status: 'success' });
+  fs.writeFile('./files/default_storage.json', JSON.stringify(req.body.products), function (err) {
+    res.status(200).json({ status: 'success' });
   });
 });
- 
+
 module.exports = products;
